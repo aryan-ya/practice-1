ae23de6fb69db4a4fcbb83af48f4831bb5aab0b5
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-09-10 22:42:00 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { BrowserRouter as Route , Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+import Services from "./components/Services";
+import Contact from "./components/Contact"
+import "./styles/header.scss";
+import "./styles/App.scss";
+
+
 
 function App() {
   return (
-   <Routes>
-    <Route></Route>
-   </Routes>
+ <Router>
+  <Header />
+  <Routes>
+     <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+  </Routes>
+ </Router>
+  
   );
 }
 
