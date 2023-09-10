287e8ffb6c19a49ae45ec9b6e5ef1123de22348f
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-09-10 19:43:31 +0530

    practice-1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import { BrowserRouter as Route , Routes } from "react-router-dom";
+
+function App() {
+  return (
+   <Routes>
+    <Route></Route>
+   </Routes>
+  );
+}
+
+export default App;
