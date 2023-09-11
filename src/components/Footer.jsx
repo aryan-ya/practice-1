85a4aaf24c916c6a36b3b4845954fbf42b559682
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-09-11 21:45:50 +0530

    project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+  return  <footer>
+    <div>
+      <h1>Techystar</h1>
+      <p>@all right reserved</p>
+    </div>
+
+
+   <div>
+    <h5>Follow Us</h5>
+    <div>
+      <a href="https//youtube.com/6packprogrammer" target={"_blank"}>Youtube</a>
+      <a href="https//instagram.com/aryan_yadav746/" target={"_blank"}>Instagram</a>
+     
+      <a href="https://github.com/aryan-ya"target={"_blank"}>Github</a>
+    </div>
+   </div>
+    </footer>
+ 
 }
 
 export default Footer
